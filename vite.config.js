e168a4f656610d669a3fd159c081bbcfdfae37fe
nharleytran <98291123+nharleytran@users.noreply.github.com>
2022-10-15 03:13:20 -0400

    everything is done1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "hw4-estore-nharleytran",
+  base: "/hw4-estore-nharleytran/",
   plugins: [react()]
 })
