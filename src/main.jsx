8403abb313cf5c8e4dc78ff891094ec7b9b3e3fe
nharleytran <98291123+nharleytran@users.noreply.github.com>
2022-10-16 15:49:38 -0400

    homepage

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ChakraProvider>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </ChakraProvider>
 );
